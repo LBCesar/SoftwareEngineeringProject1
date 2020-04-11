d89658b0ed83fa08e9eff5b341827e4d969d3d67
Sorez <shorajmanandhar@gmail.com>
2020-04-10 20:07:35 -0700

    Added Savings class

---
+++ src/softwareengineeringproject1/savingsAccount.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 
+ */
+
+public class savingsAccount {
+    
+    private double balance;
+    private int accountNumber;
+    
+
+    public void getBalance(){
+    
+    }
+    
+    
+    public void setBalance(double amount){
+    
+    }
+    
+    
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    
+    
+    public void setAccountNumber(int number){
+    }
+}
