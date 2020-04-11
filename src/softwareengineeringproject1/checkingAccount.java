9306c4d12e96ded22ed6d5a2a6b3cfc1595080cb
Sorez <shorajmanandhar@gmail.com>
2020-04-10 20:04:03 -0700

    Added Checkings class

---
+++ src/softwareengineeringproject1/checkingAccount.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+public class checkingAccount {
+    
+    private double balance;
+    private int accountNumber;
+    
+
+    public void getBalance(){
+    
+    }
+    
+    public void setBalance(double amount){
+    
+    }
+    
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    
+    public void setAccountNumber(int number){
+    
+    }
+
+}
