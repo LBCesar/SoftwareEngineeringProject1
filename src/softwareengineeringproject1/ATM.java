17934a67b9f667e0cfc15a7a32d5edd50f6265f0
Sorez <shorajmanandhar@gmail.com>
2020-04-10 19:54:43 -0700

    Added ATM.class

---
+++ src/softwareengineeringproject1/ATM.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * 
+ */
+
+import java.util.*;
+
+public class ATM {
+
+    private double amount;
+
+    public void start() {//Calls the display class.
+        display screen = new display();
+        screen.screen();
+    }
+
+    
+    public void deposit(double amount){
+
+    }
+
+    public void withdraw(double amount){
+
+    }
+
+    public void checkBalance() {
+
+    }
+
+    public void transfer(double amount){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
