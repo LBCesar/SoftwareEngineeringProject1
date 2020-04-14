6adc9546a3b92a38e22104fa5300ffae6a959f82
Sorez <shorajmanandhar@gmail.com>
2020-04-14 00:11:09 -0700

    Updated with choosing checkings/savings

--- src/softwareengineeringproject1/ATM.java
+++ src/softwareengineeringproject1/ATM.java
@@ -5,34 +5,37 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ATM {
+
     Display screen;
-    BankCustomer me=new BankCustomer();
+    BankCustomer me = new BankCustomer();
     
 
-    public ATM(){//Might not need.
-        this.me=bc();
+    public ATM(){
 
     }
-    private ATM(BankCustomer me) {//Made this constructor to load in a customer, if we can find a more graceful way to load in it would be cool.
-        System.out.println("Customer loaded in");
+    
+
+    private ATM(BankCustomer me) {      //Made this constructor to load in a customer, if we can find a more graceful way to load in it would be cool.
+        System.out.println("Customer loaded in\n");
         this.screen = new Display();
         this.me = me;
         start();
     }
 
-    public BankCustomer bc(){//just for testing, delete later.
+    public BankCustomer bc(){          //just for testing, delete later.
         return me;
     }
+
     public final void start() {//Calls the display class.
         //these next two lines are a quick way of dealing with login
         //the Display function Login() will create an array with the first element
         //being the accoutn number and the secon element being the pin
         //this way we get both in one call, if either is wrong we dont allow entry anyways.
         
-        int f[]=new int[2];
-        f=screen.Login();
-        System.out.println("Account:"+f[0]);
-        System.out.println("Pin"+f[1]);
+        int f[] = new int[2];
+        f = screen.Login();
+        System.out.println("Account Number: " + f[0]);
+        System.out.println("PIN: " + f[1]);
 
             //if(f[0]==our number)
             // if(f[1]==our pin)
@@ -41,51 +44,77 @@ public class ATM {
         
         
         //We need to pick either checking or savings, idk what the best approach is, i made this one up.
-        int x=screen.checkOrSave();
+        int accountChoice = screen.checkOrSave();
         //maybe we can have some booleans that we can use to satisfiy conditions later on.
         boolean check=false;
-        boolean save=false;   
-        if(x==1){
+        boolean save=false;
+
+        if(accountChoice == 1){
             //checking
-            check=true;
+            check = true;
+            System.out.println("Checkings Account");
         }
-        if(x==2){
+
+        else if(accountChoice == 2){
             //savings
             save=true;
+            System.out.println("Savings Account");
         }
-        System.out.println("Checkings:"+check);
-        System.out.println("Savings:"+save);
-        
-        
-        
-        //for example if we deposit and choose checking(x==1)
-        //if(check==true)
-            //our deposit will go into the checkings account
-        //if(save==true)
-            // this condition isnt met as check/save are always false.
-        
 
-        
+        // System.out.println("Checkings:" + check);
+        // System.out.println("Savings:" + save);
+              
         
         screen.mainMenu();
-        int choice=screen.screen();//Our screen is the keypad, we are waiting for input
-        //if(choice==1 2 )
-        if(choice==3){//Choices return here and we call more displays if needed
-            float amount=screen.depositDisplay();//Our depositDisplay() returns the user input for deposit amount
-            this.deposit(amount);//Now that we have the amount, call the deposit() function in ATM
+        int choice = screen.screen();   //Our screen is the keypad, we are waiting for input
+
+        if(choice == 1){                // view balance
+            screen.checkBalanceDisplay();
+            balance(accountChoice);
         }
-        //if(choice == 4 5 6 7 ....)
+
+        else if(choice == 2){
+            // Withdraw cash
+        }
+
+        else if(choice == 3){                       //Choices return here and we call more displays if needed
+            float amount = screen.depositDisplay(); //Our depositDisplay() returns the user input for deposit amount
+            this.deposit(amount, accountChoice);    //Now that we have the amount, call the deposit() function in ATM
+        }
+
+        else if(choice == 4){
+            // transfer
+        }
+
+        else{
+            System.out.println("Exit program");
+            System.exit(0);
+        }
     }
-    //void witdraw()
-    //void transfer()
-    //void balance()
-        public void deposit(float amount) {//Here i handle all the "Logic" that is needed for a deposit. 
-            //if(savings)
-            me.savingsDeposit(amount);//Call BankCustmoer->savingsDeposit()->Which calls our savingsAccount.java function addBalance();
-            //if(checkings)
-            me.checkingsDeposit(amount);
-            
-        }
+
+    public void witdraw(float amount){
+        // Shaan
+    }
+
+    public void transfer(float amount){
+        // Andre
+    }
+
+    public void balance(int accountChoice){
+        if (accountChoice == 1)
+            System.out.println("Checkings Account Balance: $" + me.getCheckingsBalance());
+
+        if (accountChoice == 2)
+            System.out.println("Savings Account Balance: $" + me.getSavingsBalance());
+    }
+
+    public void deposit(float amount, int accountChoice) {  // Here i handle all the "Logic" that is needed for a deposit. 
+        if (accountChoice == 1)
+            me.checkingsDeposit(amount);    
+        
+        if(accountChoice == 2)
+            me.savingsDeposit(amount);        // Call BankCustmoer->savingsDeposit()->Which calls our savingsAccount.java function addBalance();
+    }
 
 
     public static void main(String[] args) {
@@ -147,21 +176,16 @@ public class ATM {
        // customer.get(0).checkBalance();
         
         //================Delete Later. 
-        System.out.println("NEW\n\n\nBefore ATM: $" + customer.get(0).getCheckingsBalance());
-        System.out.println("Before ATM: $" + customer.get(0).getSavingsBalance());    
-        ATM a= new ATM(me);
-        BankCustomer bc=a.bc();
-        System.out.println("After ATM: $" + bc.getCheckingsBalance());
-        System.out.println("After ATM: $" + bc.getSavingsBalance());
+        System.out.println("NEW\n\n\nInitial Checkings Balance: $" + customer.get(0).getCheckingsBalance());
+        System.out.println("Initial Savings Balance: $" + customer.get(0).getSavingsBalance());  
+
+        ATM a = new ATM(me);
+        BankCustomer bc = a.bc();
+
+        System.out.println("After Checkings Balance: $" + bc.getCheckingsBalance());
+        System.out.println("After Savings Balance: $" + bc.getSavingsBalance());
 
     }
 
 }
 
-
-/**
- * Self Note: 
- * Log in window
- * money should be only two decimal place
- * 
- */
--- src/softwareengineeringproject1/Display.java
+++ src/softwareengineeringproject1/Display.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.*;
 
 public class Display extends ATM{
-// public class Display{
     
     public int screen() {//This is sort of now the keypad/ main landing zone.
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
@@ -17,6 +16,7 @@ public class Display extends ATM{
 
         return choice;
     }
+
     //takes in a value between low and high from the user 
 	//to make sure nothing breaks
 	public int getIntRange( int low, int high ) {
@@ -40,67 +40,54 @@ public class Display extends ATM{
 	}
 
     public int[] Login(){
-        System.out.println("PROJECT ATM\n");
+        System.out.println("Please enter your account number and PIN");
         int f[]=new int[2];
-        System.out.print("Account number:\n");//Need function for verifying.
+        System.out.print("Account Number:\n");      //Need function for verifying.
         f[0]=this.screen();
-        System.out.print("Enter pin:\n");//Need function for verifying.
+        System.out.print("Enter PIN:\n");           //Need function for verifying.
         f[1]=this.screen();
         return f;
     }
+
     public int checkOrSave(){
-        System.out.println("We need to figure out how to choose between checking and savings\nIs here a good spot?");
-        System.out.println("1)Checking\n2)Savings");
-        int f=this.screen();
+        System.out.println("\nPlease choose an account to make your transaction");
+        System.out.println("[1] Checkings Account");
+        System.out.println("[2] Savings Account");
+        int f = this.screen();
         return f;
-    }
-
-
-    public void mainDisplay(){
-
     }
 
 
     public void mainMenu(){
-        System.out.println("\n_____________________");
-        System.out.println("|     Main Menu        |");
-        System.out.println("|[1] View Balance      |");
-        System.out.println("|[2] Withdraw Cash     |");
-        System.out.println("|[3] Deposit Cash      |");
-        System.out.println("|[4] Transfer          |");
-        System.out.println("|[5] Exit              |");
-        System.out.println("|______________________|");
+        System.out.println("[1] View Balance      ");
+        System.out.println("[2] Withdraw Cash     ");
+        System.out.println("[3] Deposit Cash      ");
+        System.out.println("[4] Transfer          ");
+        System.out.println("[5] Exit              ");
     }
 
 
     public void checkBalanceDisplay() {
-        System.out.println("Display balance here");
-        //checkings or savings?
+        System.out.println("Display Current Balance");          // Could be possible to print current system time format
     }
 
     public float depositDisplay() {
         System.out.println("Deposit amount:");
-        //float amount=50;//Replace this with user input.
-        //checkings or savings?
-        float amount=this.screen();
+        float amount = this.screen();
         return amount;
     }
 
     public void withdrawDisplay() {
-        System.out.println(" _____________________");
         System.out.println("|[1] $20      [5] $100|");
         System.out.println("|[2] $40      [6] $120|");
         System.out.println("|[3] $60      [7] $140|");
         System.out.println("|[4] $80      [8] Exit|");
-        System.out.println("|_____________________|");
-        //checking or savings?
-
     }
 
     public void transferDisplay(BankCustomer me) {
         System.out.println("Which account do you wish to transfer money to:");
-        System.out.println("1. Checkings to Savings");
-        System.out.println("2. Savings to Checkings");
+        System.out.println("[1] Checkings to Savings");
+        System.out.println("[2] Savings to Checkings");
         
         int choice = getIntRange(1,2);
         int amount = 0;
