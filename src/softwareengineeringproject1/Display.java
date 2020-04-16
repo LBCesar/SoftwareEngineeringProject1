2cc5b0e693a7bbce7adb3880fd01d616e5a3632e
acortes417 <acortes417@gmail.com>
2020-04-15 19:14:05 -0700

    Update Display.java

--- src/softwareengineeringproject1/Display.java
+++ src/softwareengineeringproject1/Display.java
@@ -7,92 +7,93 @@ import java.util.*;
 
 public class Display extends ATM{
     
-    public int screen() {//This is sort of now the keypad/ main landing zone.
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        int choice = myObj.nextInt();
-        return choice;
-    }
-
-    //takes in a value between low and high from the user 
-	//to make sure nothing breaks
-	public int getIntRange( int low, int high ) {
-		Scanner in = new Scanner( System.in );
-		int input = 0;
-		boolean valid = false;
-		while( !valid ) {
-			if( in.hasNextInt() ) {
-				input = in.nextInt();
-				if( input <= high && input >= low ) {
-					valid = true;
+	    public int screen() {//This is sort of now the keypad/ main landing zone.
+	        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	        int choice = myObj.nextInt();
+	        return choice;
+	    }
+	
+	    //takes in a value between low and high from the user 
+		//to make sure nothing breaks
+		public int getIntRange( int low, int high ) {
+			Scanner in = new Scanner( System.in );
+			int input = 0;
+			boolean valid = false;
+			while( !valid ) {
+				if( in.hasNextInt() ) {
+					input = in.nextInt();
+					if( input <= high && input >= low ) {
+						valid = true;
+					} else {
+						System.out.println( "Invalid Range." );
+					}
 				} else {
-					System.out.println( "Invalid Range." );
+					in.next(); //clear invalid string
+					System.out.println( "Invalid Input." );
 				}
-			} else {
-				in.next(); //clear invalid string
-				System.out.println( "Invalid Input." );
 			}
+			return input;
 		}
-		return input;
+	
+	    public int[] Login(){
+	        System.out.println("Please enter your account number and PIN");
+	        int f[]=new int[2];
+	        System.out.print("Account Number(1234): ");
+	        f[0]=this.screen();
+	        System.out.print("Enter PIN(1111): ");
+	        f[1]=this.screen();
+	        return f;
+	    }
+	    
+	    public int rerun(){
+	        System.out.println("Would you like to make another transaction?");
+	        System.out.println("[1] Yes\n[2] No");
+	        return this.screen();
+	    }
+	
+	    public int checkOrSave(){
+	        System.out.println("\nPlease choose an account to make your transaction");
+	        System.out.println("[1] Checkings Account");
+	        System.out.println("[2] Savings Account");
+	        int f = this.screen();
+	        return f;
+	    }
+	
+	    public void mainMenu(){
+	        System.out.println("[1] View Balance      ");
+	        System.out.println("[2] Withdraw Cash     ");
+	        System.out.println("[3] Deposit Cash      ");
+	        System.out.println("[4] Transfer          ");
+	        System.out.println("[5] Exit              ");
+	    }
+	
+	
+	    public void checkBalanceDisplay() {
+	        System.out.println("Display Current Balance");          // Could be possible to print current system time format
+	    }
+	
+	    public float depositDisplay() {
+	        System.out.println("Deposit amount:");
+	        float amount = this.screen();
+	        return amount;
+	    }
+	
+	    public int withdrawDisplay() {
+	        System.out.println("|[1] $20      [5] $100|");
+	        System.out.println("|[2] $40      [6] $120|");
+	        System.out.println("|[3] $60      [7] $140|");
+	        System.out.println("|[4] $80      [8] Exit|");
+	        int choice = this.screen();
+	        return choice;
+	    }
+	
+	    public int transferDisplay() {
+	        System.out.println("Which account do you wish to transfer money to:");
+	        System.out.println("[1] Checkings to Savings");
+	        System.out.println("[2] Savings to Checkings");
+	        
+	        int choice = getIntRange(1,2);
+	        return choice;
+	
 	}
-
-    public int[] Login(){
-        System.out.println("Please enter your account number and PIN");
-        int f[]=new int[2];
-        System.out.print("Account Number(1234): ");
-        f[0]=this.screen();
-        System.out.print("Enter PIN(1111): ");
-        f[1]=this.screen();
-        return f;
-    }
-    
-    public int rerun(){
-        System.out.println("Would you like to make another transaction?");
-        System.out.println("[1] Yes\n[2] No");
-        return this.screen();
-    }
-
-    public int checkOrSave(){
-        System.out.println("\nPlease choose an account to make your transaction");
-        System.out.println("[1] Checkings Account");
-        System.out.println("[2] Savings Account");
-        int f = this.screen();
-        return f;
-    }
-
-    public void mainMenu(){
-        System.out.println("[1] View Balance      ");
-        System.out.println("[2] Withdraw Cash     ");
-        System.out.println("[3] Deposit Cash      ");
-        System.out.println("[4] Transfer          ");
-        System.out.println("[5] Exit              ");
-    }
-
-
-    public void checkBalanceDisplay() {
-        System.out.println("Display Current Balance");          // Could be possible to print current system time format
-    }
-
-    public float depositDisplay() {
-        System.out.println("Deposit amount:");
-        float amount = this.screen();
-        return amount;
-    }
-
-    public int withdrawDisplay() {
-        System.out.println("|[1] $20      [5] $100|");
-        System.out.println("|[2] $40      [6] $120|");
-        System.out.println("|[3] $60      [7] $140|");
-        System.out.println("|[4] $80      [8] Exit|");
-        int choice = this.screen();
-        return choice;
-    }
-
-    public int transferDisplay() {
-        System.out.println("Which account do you wish to transfer money to:");
-        System.out.println("[1] Checkings to Savings");
-        System.out.println("[2] Savings to Checkings");
-        
-        int choice = getIntRange(1,2);
-        return choice;
-
 }
