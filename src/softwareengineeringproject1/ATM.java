6c776fd3376543d34dafecfe8c97cfc7715e7f04
acortes417 <acortes417@gmail.com>
2020-04-27 10:40:11 -0700

    Update ATM.java

--- src/softwareengineeringproject1/ATM.java
+++ src/softwareengineeringproject1/ATM.java
@@ -1,218 +1,185 @@
+import java.text.DecimalFormat;
+import java.util.*;
 
-public class ATM {
+public class Display {
 
-    Display screen = new Display();
-    BankCustomer me = new BankCustomer();
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    DecimalFormat df2 = new DecimalFormat("#.##");
 
-    private ATM(BankCustomer me) {
-        this.me = me;
-        start();
+    public int screen(int low, int high) {//This is sort of now the keypad/ main landing zone.
+    	int input = 0;
+		boolean valid = false;
+		while( !valid ) {
+			if( myObj.hasNextInt() ) {
+				input = myObj.nextInt();
+				if( input <= high && input >= low ) {
+					valid = true;
+				} else {
+					System.out.println( "Invalid Range." );
+				}
+			} else {
+				myObj.next(); //clear invalid string
+				System.out.println( "Invalid Input." );
+			}
+		}
+		return input;
     }
-
-
-    public final void start() {
-        //Login
-        if (!this.verifyLogin()) {
-            screen.exit();
-            System.exit(0);
-        }
-        //Loop
-        while (true) {
-            //Do we want checkings or savings?
-            int accountChoice = screen.checkOrSave();
-            //Print the main menu
-            int choice = screen.mainMenu();
-            switch (choice) {
-                case 1://Balance
-                    balance(accountChoice);
-                    break;
-                case 2://Withdraw
-                    withdraw(accountChoice);
-                    break;
-                case 3://Deposit
-                    deposit(accountChoice);
-                    break;
-                case 4://Transfer
-                    transfer(accountChoice);
-                    break;
-                default://Exit
-                    screen.exit();
-                    System.exit(0);
-            }
-            //Do we want to run again ?
-            if (screen.rerun() != 1) {
-                screen.exit();
-                System.exit(0);
-            }
-        }
-    }
-    private boolean verifyLogin() {
-        boolean clear = false;
-        int tries = 0;
+    public int[] loginDisplay() {
+    	System.out.println("****************************************************************");
+        System.out.println("* Please enter your account number and PIN                     *");
         int f[] = new int[2];
-        while (tries < 3) {
-            f = screen.loginDisplay();
-            if (f[0] == me.getCardNumber()) {
-                if (f[1] == me.getPIN()) {
-                    clear = true;
-                    break;
-                }
-            }
-            screen.loginError();
-            tries++;
-        }
-        return clear;
+        System.out.printf("%-63s*\n","* Account Number(1234):");
+        f[0] = this.screen(0, 9999);
+        System.out.printf("%-63s*\n","* Enter PIN(1111):");
+        f[1]=this.screen(0, 9999);
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************\n\n");
+        return f;
     }
-    
-    public void withdraw(int accountChoice) {
-    	boolean suf = false;
-    	while(!suf) {
-	        int amount = 0;
-	        int choiceWithdraw = screen.withdrawDisplay();	//prompts for an input for withdraw amount
-                switch (choiceWithdraw) {
-                    case 1:
-                        //choice 1 will withdraw $20
-                        amount = 20;
-                        break;
-                    case 2:
-                        //choice 2 will withdraw $40
-                        amount = 40;
-                        break;
-                    case 3:
-                        //choice 3 will withdraw $60
-                        amount = 60;
-                        break;
-                    case 4:
-                        //choice 4 will withdraw $80
-                        amount = 80;
-                        break;
-                    case 5:
-                        //choice 5 will withdraw $100
-                        amount = 100;
-                        break;
-                    case 6:
-                        //choice 6 will withdraw $120
-                        amount = 120;
-                        break;
-                    case 7:
-                        //choice 7 will withdraw $140
-                        amount = 140;
-                        break;
-                    default:
-                        //any other choice will withdraw $0 and ask to go back to menu
-                        amount = 0;
-                        break;
-                }
+
+    public int rerun() {
+    	System.out.println("****************************************************************");
+        System.out.println("*          Would you like to make another transaction?         *");
+        System.out.println("* [1] Yes                                                      *");
+        System.out.println("* [2] No                                                       *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************");
+        return this.screen(1, 2);
+    }
+
+    public int checkOrSave() {
+    	System.out.println("****************************************************************");
+    		
+    	System.out.println("*                  Welcome to our ATM                          *");		
+        System.out.println("*                                                              *"
+        		+          "\n* Please choose an account to make your transaction            *");
+        System.out.println("*   [1] Checkings Account                                      *");
+        System.out.println("*   [2] Savings Account                                        *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************");
+        int f = this.screen(1, 2);
+
+        if(f == 1)
+            System.out.println("\nCheckings Account");
+        else
+            System.out.println("\nSavings Account");
+
+        return f; 
+    }
+
+    public int mainMenu() {
+    	System.out.println("****************************************************************");
+    	System.out.println("*                      Banking Options                         *");
+    	System.out.println("*                                                              *");
+        System.out.println("* [1] View Balance                                             *");
+        System.out.println("* [2] Withdraw Cash                                            *");
+        System.out.println("* [3] Deposit Cash                                             *");
+        System.out.println("* [4] Transfer                                                 *");
+        System.out.println("* [5] Exit                                                     *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************\n\n");
+        return this.screen(1, 5);
+    }
+
+    public void checkBalanceDisplay(int accountChoice,double amount) {
+    	
+        if (accountChoice == 1) {
+        	System.out.println("****************************************************************");
+        	System.out.println("*                                                              *");
+            System.out.printf("%-63s*\n","* Checking Balance: $" + df2.format(amount));
+            System.out.println("*                                                              *");
+            System.out.println("****************************************************************\n\n");
+        }
+
+        if (accountChoice == 2) {
+        	System.out.println("****************************************************************");
+        	System.out.println("*                                                              *");
+            System.out.printf("%-63s*\n","* Savings Balance: $" + df2.format(amount));
+            System.out.println("*                                                              *");
+            System.out.println("****************************************************************\n\n");
+        }
+    }
+
+    public float depositDisplay() {
+    	System.out.println("****************************************************************");
+    	System.out.println("*                     Enter Deposit Amount                     *");
+        System.out.println("* Deposit Amount:                                              *");
+        float amount = this.screen(1, 9999);
+        System.out.println("*                                                              *");
+    	
+        return amount;
+    }
+
+    public int withdrawDisplay() {
+
+        System.out.println("****************************************************************");
+        System.out.println("*                        Withdraw Amount                       *");
+        System.out.println("*                                                              *");
+        System.out.println("*  |[1] $20      [5] $100|                                     *");
+        System.out.println("*  |[2] $40      [6] $120|                                     *");
+        System.out.println("*  |[3] $60      [7] $140|                                     *");
+        System.out.println("*  |[4] $80      [8] Exit|                                     *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************");
+        int choice = this.screen(1, 8);
+        return choice;
+    }
+
+    public double transferDisplay(int choice, BankCustomer me) 
+    {
+		
+		int amount = 0;
+		double balance = 0;
+		System.out.println("*                                                              *");
 	
-	        if (accountChoice == 1) {
-	            if (me.getCheckingsBalance() - amount >= 0) {	//checks for sufficient funds
-	                me.checkingsWithdraw(amount);
-	                suf = true;
-	            } else {
-	                screen.fundsError();
-	            }
-	        }
-	        if (accountChoice == 2) {
-	            if (me.getSavingsBalance() - amount >= 0) {		//checks for sufficient funds
-	                me.savingsWithdraw(amount);
-	                suf = true;
-	            } else {
-	                screen.fundsError();
-	            }
-	        }
-    	}
+		if(choice == 1){
+			balance = me.getCheckingsBalance();
+			System.out.println("* Transferring from Checkings to Savings                       *");
+		}
+	
+		else{
+			balance = me.getSavingsBalance();
+			System.out.println("* Transferring from Savings to Checkings                       *");
+		}
+	
+		System.out.println("* Enter the amount you wish to tranfer:                        *");
+		amount = this.screen(1, Integer.MAX_VALUE);
+		while(!(balance - amount >= 0))
+		{
+			System.out.printf("%-63s*\n","* You do not have sufficient funds, enter a different amount: ");
+			amount = this.screen(1,Integer.MAX_VALUE);
+		}
+		System.out.println("*                                                              *");
+		return amount;
     }
-
-    public void transfer(int accountChoice){        // andre look at this. idk if this makes proper sense.
-
-        double amount;
-
-        if(accountChoice == 1) 
-        {
-            // amount = getIntRange(0, Integer.MAX_VALUE);
-            amount = screen.transferDisplay(accountChoice);
-            if(me.getCheckingsBalance() - amount >= 0)
-            {
-                me.getCheckingAccount().minusBalance(amount);
-                me.getSavingsAccount().addBalance(amount);
-                // me.checkingsWithdraw(amount);
-                // me.savingsDeposit(amount);
-            }
-            else
-            {
-                screen.fundsError();
-            }
-        }
-
-        else if(accountChoice == 2)
-        {
-            // System.out.println("Enter the amount you wish to tranfer: ");
-            // amount = getIntRange(0, Integer.MAX_VALUE);
-            amount = screen.transferDisplay(accountChoice);
-            if(me.getSavingsBalance() - amount >= 0)
-            {
-
-                me.getCheckingAccount().addBalance(amount);
-                me.getSavingsAccount().minusBalance(amount);
-                // me.checkingsDeposit(amount);
-                // me.savingsWithdraw(amount);
-            }
-            else
-            {
-                screen.fundsError();
-            }
-        }
+    public void dipalyBalances(double saveBal, double checkBal)
+    {
+    	
+    	System.out.println("****************************************************************");
+    	System.out.println("*                                                              *");
+    	System.out.printf("%-63s*\n","* Checking Balance: $" + df2.format(checkBal));
+        System.out.printf("%-63s*\n","* Savings Balance: $" + df2.format(saveBal));
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************\n\n");
     }
-
-//    public void balance(int accountChoice) {
-//        screen.checkBalanceDisplay();
-//        if (accountChoice == 1) {
-//            System.out.println("Checkings Balance: $" + me.getCheckingsBalance());
-//        }
-//
-//        if (accountChoice == 2) {
-//            System.out.println("Savings Balance: $" + me.getSavingsBalance());
-//        }
-//    }
-    public void balance(int accountChoice) {
-        if (accountChoice == 1) {
-            screen.checkBalanceDisplay(accountChoice,me.getCheckingsBalance());
-        }
-
-        if (accountChoice == 2) {
-            screen.checkBalanceDisplay(accountChoice,me.getSavingsBalance());
-
-        }
+    public void loginError(){
+    	System.out.println("\n\n****************************************************************");
+        System.out.printf("%-63s*\n","*                   Incorrect Account number or PIN!");
+        System.out.println("****************************************************************\n\n");
     }
+    public void fundsError(){
 
-    public void deposit(int accountChoice) {
-        float amount = screen.depositDisplay(); //Our depositDisplay() returns the user input for deposit amount
-        if (accountChoice == 1) {
-            me.checkingsDeposit(amount);
-        }
-
-        if (accountChoice == 2) {
-            me.savingsDeposit(amount);
-        }
+    	System.out.println("\n\n****************************************************************");
+        System.out.printf("%-63s*\n","*                   Insufficient Funds!");
+        System.out.println("****************************************************************\n\n");
     }
-
-    public static void main(String[] args) {
-
-        BankCustomer me = new BankCustomer();
-
-        me.setName("lil Kanye");
-        me.setEmail("dontgiveahoot@hoot.hoo");
-        me.setAddress("1919 KayneWest@jtriseltunoob");
-        me.setCardNumber(1234);
-        me.setPIN(1111);
-        me.setCheckingsAccountNumber(4545);
-        me.setCheckingsBalance(150);
-        me.setSavingsAccountNumber(4546);
-        me.setSavingsBalance(100);
-
-
-        ATM a = new ATM(me);
-
+    public void exit(double cBal, double sBal){
+        System.out.println("****************************************************************");
+    	System.out.println("*                 Have A Nice Day                              *");
+    	System.out.println("*                                                              *");
+    	System.out.printf("%-63s*\n","* Checking Balance: $" + df2.format(cBal));
+        System.out.printf("%-63s*\n","* Savings Balance: $" + df2.format(sBal));
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************\n\n");
     }
-
 }
